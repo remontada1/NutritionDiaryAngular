49c24872dfad0ce5b3f12a232cf6052b2505ea05
Vitalii <vitalii.meshcheriakov@gmail.com>
2018-07-16 19:24:51 +0300

    Add takeUntil and ngOnDestroy to app.component for unsubscribing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
+import { Subject } from 'rxjs/Rx';
 import { UserService } from './user/user.service';
 
 
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
@@ -14,16 +15,27 @@ import { Observable } from '../../node_modules/rxjs/Observable';
     styleUrls: ['app.component.css'],
     providers: [UserService]
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit, OnDestroy {
+    private unsubscribe$ = new Subject();
     private isLoggedIn: boolean;
 
 
     constructor(private router: Router, private userService: UserService) {
         // this.isLoggedIn = userService.isLoggedIn();
-        userService.isLoggedIn$.subscribe((isLoggedIn) =>
-            this.isLoggedIn = isLoggedIn
-        );
+        userService.isLoggedIn$
+            .takeUntil(this.unsubscribe$)
+            .subscribe((isLoggedIn) =>
+                this.isLoggedIn = isLoggedIn
+            );
+    }
+
+    ngOnInit() {
+
+    }
+
+    ngOnDestroy(): void {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
     }
 
 }
